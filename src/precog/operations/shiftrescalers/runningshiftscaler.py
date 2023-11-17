60b41fac57e4b06b0e88c7544cefb519f29ce3ba
Anthony Fong <FongAnthonyM@gmail.com>
2023-11-16 16:11:49 -0800

    Added modified zscoring and threshold parameter

--- src/precog/operations/shiftrescalers/runningshiftscaler.py
+++ src/precog/operations/shiftrescalers/runningshiftscaler.py
@@ -33,7 +33,7 @@ blank_arg = object()
 class RunningShiftScaler(BaseOperation):
     default_input_names: tuple[str, ...] = ("data",)
     default_output_names: tuple[str, ...] = ("ss_data",)
-    default_shift_rescale: str = "shift_rescale_zscore"
+    default_shift_rescale: str = "shift_rescale_modified_zscore"
     default_forget: str = "exponential_forget"
 
     # Magic Methods #
@@ -44,6 +44,7 @@ class RunningShiftScaler(BaseOperation):
         forget_factor: float | None | object = blank_arg,
         mean: np.ndarray | None = None,
         variance: np.ndarray | None = None,
+        threshold: int | float | None = None,
         axis: int | None = None,
         *args: Any,
         init_io: bool = True,
@@ -60,6 +61,8 @@ class RunningShiftScaler(BaseOperation):
 
         self.previous_variance: np.ndarray | None = None
         self.previous_variance_fill_value: float | int = 0
+
+        self.threshold: int | float = 10 ** 12
 
         self.previous_count: int = 0
 
@@ -78,6 +81,7 @@ class RunningShiftScaler(BaseOperation):
                 forget_factor,
                 mean,
                 variance,
+                threshold,
                 axis,
                 *args,
                 init_io=init_io,
@@ -107,6 +111,7 @@ class RunningShiftScaler(BaseOperation):
         forget_factor: float | None | object = blank_arg,
         mean: np.ndarray | None = None,
         variance: np.ndarray | None = None,
+        threshold: int | float | None = None,
         axis: int | None = None,
         *args: Any,
         init_io: bool = True,
@@ -137,6 +142,9 @@ class RunningShiftScaler(BaseOperation):
 
         if variance is not None:
             self.previous_variance = variance
+
+        if threshold is not None:
+            self.threshold = threshold
 
         # Construct Parent #
         super().construct(*args, init_io=init_io, sets_up=sets_up, setup_kwargs=setup_kwargs, **kwargs)
@@ -221,13 +229,36 @@ class RunningShiftScaler(BaseOperation):
             self.previous_mean = (1 - forget_factor) * self.previous_mean + forget_factor * new_data
 
             # Update Variance
-            # delta_variance = shifted_data * (new_data - self.previous_mean) - self.previous_variance
-            # self.previous_variance = (1 - forget_factor) * self.previous_variance + forget_factor * delta_variance
-
-            # delta_variance = shifted_data * (new_data - self.previous_mean)
-            # self.previous_variance = (1 - 2 * forget_factor) * self.previous_variance + delta_variance
-
             delta_variance = shifted_data * (new_data - self.previous_mean)
+            self.previous_variance = (1 - forget_factor) * self.previous_variance + forget_factor * delta_variance
+
+        self.previous_count += data.shape[-1]
+        return scaled_data
+
+    def shift_rescale_modified_zscore(self, data) -> np.ndarray:
+        scaled_data = np.empty_like(data)
+        slices = [slice(None)] * len(data.shape)
+        for i in range(data.shape[self.axis]):
+            slices[self.axis] = i
+            t_slices = tuple(slices)
+            forget_factor = self.forget(index=i)
+            new_data = data[t_slices]
+
+            # Get the Deviation from the Previous Mean
+            data_deviation = new_data - self.previous_mean
+
+            # Calculate Modified Z-score
+            data_std = np.sqrt(self.previous_variance)
+            modified_zscore = self.threshold * np.tanh(data_deviation / (self.threshold * data_std))
+            modified_deviation = modified_zscore * data_std
+            modified_data = modified_deviation + self.previous_mean
+            scaled_data[t_slices] = modified_zscore
+
+            # Update Mean
+            self.previous_mean = (1 - forget_factor) * self.previous_mean + forget_factor * modified_data
+
+            # Update Variance
+            delta_variance = modified_deviation * (modified_data - self.previous_mean)
             self.previous_variance = (1 - forget_factor) * self.previous_variance + forget_factor * delta_variance
 
         self.previous_count += data.shape[-1]
--- tests/test_datastreamer.py
+++ tests/test_datastreamer.py
@@ -24,6 +24,7 @@ import pytest
 import numpy as np
 from ucsfbids import Subject
 from h5py import Dataset
+import matplotlib.pyplot as plt
 
 # Local Packages #
 from src.precog.operations import CDFSStreamer
@@ -54,12 +55,12 @@ class ClassTest:
 
 class TestCDFSStreamer(ClassTest):
     subjects_root = pathlib.Path("/data_store0/human/converted_clinical")
-    subject_id = "EC0212"
+    subject_id = "EC0300"
 
     def test_evaluate_stream(self):
         from xltektools.xltekucsfbids import IEEGXLTEK
-        start = datetime.datetime(1970, 1, 6, 0, 0, tzinfo=datetime.timezone.utc)
-        stop = datetime.datetime(1970, 1, 6, 1, 0, tzinfo=datetime.timezone.utc)
+        start = datetime.datetime(1970, 1, 7, 0, 5, 0, tzinfo=datetime.timezone.utc)
+        stop = datetime.datetime(1970, 1, 7, 0, 9, 10, tzinfo=datetime.timezone.utc)
 
         bids_subject = Subject(name=self.subject_id, parent_path=self.subjects_root)
         session = bids_subject.sessions["clinicalintracranial"]
@@ -68,7 +69,105 @@ class TestCDFSStreamer(ClassTest):
         streamer = CDFSStreamer(cdfs=cdfs)
         streamer.setup(start=start, stop=stop, step=10, approx=True, tails=True)
 
-        outs = [streamer.evaluate() for i in range(360)]
+        outs = []
+        while (segment := streamer.evaluate()) is not None:
+            outs.append(segment)
+
+        shapes = [out.shape for out in outs]
+
+        iter_concat = np.concatenate(outs, axis=0)
+        t_iter = np.arange(0, iter_concat.shape[0])
+
+        fig, ax = plt.subplots()
+        ax.set(xlabel='time (s)', ylabel='voltage (uV)', title='Iter Data')
+        ax.plot(t_iter, iter_concat[:, 0])
+
+        plt.show()
+
+        assert True
+
+    def test_evaluate_stream_consistency(self):
+        from xltektools.xltekucsfbids import IEEGXLTEK
+        start = datetime.datetime(1970, 1, 7, 0, 1, 0, tzinfo=datetime.timezone.utc)
+        stop = datetime.datetime(1970, 1, 7, 0, 1, 30, tzinfo=datetime.timezone.utc)
+
+        bids_subject = Subject(name=self.subject_id, parent_path=self.subjects_root)
+        session = bids_subject.sessions["clinicalintracranial"]
+        cdfs = session.modalities["ieeg"].require_cdfs()
+
+        streamer = CDFSStreamer(cdfs=cdfs)
+        streamer.setup(start=start, stop=stop, step=1, approx=True, tails=True)
+
+        all_data = cdfs.data.find_data_slice(start=start, stop=stop)
+        data = np.array(all_data[0])
+
+        outs = []
+        while (seg := streamer.evaluate()) is not None:
+            outs.append(seg)
+
+        iter_concat = np.concatenate(outs, axis=0)
+
+        t_iter = np.arange(0, iter_concat.shape[0])
+        t_data = np.arange(0, data.shape[0])
+
+        fig, ax = plt.subplots()
+        ax.set(xlabel='time (s)', ylabel='voltage (uV)', title='Iter Data Blue, Data Orange')
+        ax.plot(t_iter, iter_concat[:, 0])
+        ax.plot(t_data, data[:, 0])
+
+        fig, ax = plt.subplots()
+        ax.set(xlabel='time (s)', ylabel='voltage (uV)', title='Iter Data')
+        ax.plot(t_iter, iter_concat[:, 0])
+
+        fig, ax = plt.subplots()
+        ax.set(xlabel='time (s)', ylabel='voltage (uV)', title='Data')
+        ax.plot(t_data, data[:, 0])
+
+        plt.show()
+
+        assert True
+
+    def test_evaluate_stream_overlap(self):
+        from xltektools.xltekucsfbids import IEEGXLTEK
+        start_1 = datetime.datetime(1970, 1, 7, 0, 1, 0, tzinfo=datetime.timezone.utc)
+        stop_1 = datetime.datetime(1970, 1, 7, 0, 1, 30, tzinfo=datetime.timezone.utc)
+        start_2 = datetime.datetime(1970, 1, 7, 0, 1, 10, tzinfo=datetime.timezone.utc)
+        stop_2 = datetime.datetime(1970, 1, 7, 0, 1, 40, tzinfo=datetime.timezone.utc)
+
+        bids_subject = Subject(name=self.subject_id, parent_path=self.subjects_root)
+        session = bids_subject.sessions["clinicalintracranial"]
+        cdfs = session.modalities["ieeg"].require_cdfs()
+
+        streamer = CDFSStreamer(cdfs=cdfs)
+        streamer.setup(start=start_1, stop=stop_1, step=1, approx=True, tails=True)
+
+        outs = []
+        while (seg := streamer.evaluate()) is not None:
+            outs.append(seg)
+
+        iter_concat_1 = np.concatenate(outs, axis=0)
+
+        streamer2 = CDFSStreamer(cdfs=cdfs)
+        streamer2.setup(start=start_2, stop=stop_2, step=1, approx=True, tails=True)
+
+        outs2 = []
+        while (seg := streamer2.evaluate()) is not None:
+            outs2.append(seg)
+
+        iter_concat_2 = np.concatenate(outs2, axis=0)
+
+        t_iter_1 = np.arange(0, 1024*30)
+        t_iter_2 = np.arange(10240, 1024*40)
+
+        fig, ax = plt.subplots(2, 1)
+        ax[0].set(xlabel='time (s)', ylabel='voltage (uV)', title='Two iterators')
+        ax[0].plot(t_iter_1, iter_concat_1[:, 0])
+        ax[0].plot(t_iter_2, iter_concat_2[:, 0])
+
+        ax[1].plot(t_iter_2, iter_concat_2[:, 0])
+        ax[1].plot(t_iter_1, iter_concat_1[:, 0])
+
+        plt.show()
 
         assert True
 
